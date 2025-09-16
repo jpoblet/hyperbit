ceaf6b73fcb79d9410f1ba588ce083cb4e3dfc66
jpoblet <jpobletp@gmail.com>
2025-09-16 21:51:47 +0000

    Responsiveness of the HeaderTitle & Subtitle

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = ({
 }: HeroSectionProps) => {
   return (
     <section className="flex content-end flex-wrap relative w-full min-h-[80vh] bg-transparent text-foreground overflow-hidden">
-      <div className="px-16 py-14 z-10">
+      <div className="px-8 py-6 md:px-12 md:py-10 lg:px-14 lg:py-12 z-10">
         <div className="flex flex-col gap-12 max-w-3xl h-full">
           <h1 className="text-6xl md:text-7xl lg:text-8xl">{title}</h1>
           <p className="text-lg md:text-xl mb-8 text-foreground-secondary">
