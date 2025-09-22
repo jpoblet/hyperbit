8b69287eccbe6a23a6a66fa02ef4279252855e28
jpoblet <jpobletp@gmail.com>
2025-09-22 14:01:18 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: process.end.VITE_BASE_PATH || "/hyperbit",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
@@ -23,5 +23,5 @@ export default defineConfig({
   server: {
     // @ts-ignore
     allowedHosts: process.env.TEMPO === "true" ? true : undefined,
-  }
+  },
 });
